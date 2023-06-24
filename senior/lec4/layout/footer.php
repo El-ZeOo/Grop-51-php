8eb970271123b66a24f30a78611e7198f70dd34d
El-ZeOo <78923097+El-ZeOo@users.noreply.github.com>
2023-06-24 13:12:48 +0300

    Delete senior/lec4 directory

--- senior/lec4/layout/footer.php
+++
@@ -1,7 +0,0 @@
-<script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-
-
-<script src="style/bootstrap.min.css"></script>
-</body>
-
-</html>
