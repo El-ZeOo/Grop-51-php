5dd71aeca63f9911ec40baf14c02928823dcd867
El-ZeOo <78923097+El-ZeOo@users.noreply.github.com>
2023-06-24 13:12:34 +0300

    Delete senior/lec2 directory

--- senior/lec2/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$username = 'ziad';
-$age = 28;
-$result = $username == 'ziad' ? ($age == 28 ? 'yes iam ziad and my age is 28' : 'yes iam ziad but my age not 28') :  'sorry';
-echo $result;
