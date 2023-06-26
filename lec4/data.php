7cd61e9626a5205e9c0f976278fbbb0625f5c168
El-ZeOo <78923097+El-ZeOo@users.noreply.github.com>
2023-06-26 21:53:48 +0300

    lec4

---
+++ lec4/data.php
@@ -0,0 +1,9 @@
+<?php
+
+$users = [
+    'user1' => ['id' => 1, 'username' => 'adminsenior', 'email' => 'amdmin@gmail.com', 'password' => 'Admin123'],
+    'user2' => ['id' => 2, 'username' => 'ziadsenior', 'email' => 'ziad@gmail.com', 'password' => 'ziad123Z'],
+    'user3' => ['id' => 3, 'username' => 'fargsenior', 'email' => 'farg@gmail.com', 'password' => 'farg123F'],
+    'user4' => ['id' => 4, 'username' => 'mahmoudsenior', 'email' => 'mahmoud@gmail.com', 'password' => 'mahmoud123M'],
+    'user5' => ['id' => 5, 'username' => 'salamasenior', 'email' => 'salama@gmail.com', 'password' => 'salama123S'],
+];
